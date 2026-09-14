3488511857de25ed532de827315c66932d63f035
Gannu5704Q <sujit7207@gmail.com>
2026-09-14 15:05:30 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/gannu5704q/vulkanmalizink/MainActivity.java
@@ -0,0 +1,12 @@
+package com.gannu5704q.vulkanmalizink;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
